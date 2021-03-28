5bb2d951bd3f510e1eabd46ac96e8b39ed4185f0
mislavto <zivadinovictomislav@gmail.com>
2021-03-28 23:57:29 +0200

    updated BlinkerApplication.java

--- src/main/java/rs/prod/blinker/BlinkerApplication.java
+++ src/main/java/rs/prod/blinker/BlinkerApplication.java
@@ -2,9 +2,11 @@ package rs.prod.blinker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlinkerApplication {
+public class BlinkerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlinkerApplication.class, args);
